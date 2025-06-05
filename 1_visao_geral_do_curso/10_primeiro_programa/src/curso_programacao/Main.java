e862559ae069e7a88df5fba0d3a17ec169ffe82d
Lucca.Viola <luccaviola05@gmail.com>
2025-06-05 10:55:22 -0400

    Meu primeiro 'Olá mundo' em JAVA

---
+++ 1_visao_geral_do_curso/10_primeiro_programa/src/curso_programacao/Main.java
@@ -0,0 +1,13 @@
+package curso_programacao;
+
+	public class Main {
+	
+		public static void main(String[] args) {
+			
+			System.out.println("Olá mundo!");
+			System.out.println("Aprendendo essa linguagem chamada Java.");
+			System.out.println("Precisei de 14 fucking lines para printar isso!");
+			
+		}
+	
+	}
