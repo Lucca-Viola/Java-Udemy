5e7f06213d188484b2b03d151ff2020448f8213d
Lucca Viola <luccaviola05@gmail.com>
2025-06-10 22:13:11 -0400

    Desafio 1004

---
+++ secao_3_estrutura_sequencial/src/_24_exercicios_URI_online_judge/_1004_.java
@@ -0,0 +1,21 @@
+package _24_exercicios_URI_online_judge;
+
+import java.util.Scanner;
+
+public class _1004_ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		int PROD = A * B;
+		
+		System.out.printf("PROD = %d%n", PROD);
+		
+		sc.close();
+	}
+
+}
