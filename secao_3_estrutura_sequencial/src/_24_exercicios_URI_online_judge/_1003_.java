9a39bfe0f5809f4620dacbe5e81e9b728fbecb8f
Lucca Viola <luccaviola05@gmail.com>
2025-06-10 22:08:20 -0400

    Desafio 1003

---
+++ secao_3_estrutura_sequencial/src/_24_exercicios_URI_online_judge/_1003_.java
@@ -0,0 +1,21 @@
+package _24_exercicios_URI_online_judge;
+
+import java.util.Scanner;
+
+public class _1003_ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		int SOMA = A + B;
+		
+		System.out.printf("SOMA = %d%n", SOMA);
+		
+		sc.close();
+	}
+
+}
