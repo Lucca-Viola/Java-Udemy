4f4ef427ef5ec7af09d9b2d5fcfcfe61816e4362
Lucca Viola <luccaviola05@gmail.com>
2025-06-10 21:58:14 -0400

    Desafio 1001

---
+++ secao_3_estrutura_sequencial/src/_24_exercicios_URI_online_judge/_1001_.java
@@ -0,0 +1,20 @@
+package _24_exercicios_URI_online_judge;
+
+import java.util.Scanner;
+
+public class _1001_ {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+
+		int X = A + B;
+		
+		System.out.printf("X = %d%n", X);
+		
+		sc.close();
+	}
+
+}
