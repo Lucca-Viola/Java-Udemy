c6a7a27d563513171c5f2ecca227e276df29980d
Lucca Viola <luccaviola05@gmail.com>
2025-06-14 13:47:12 -0400

    Desafio 1036

---
+++ secao_4_estrutura_condicional/src/_35/exercicio_dois/Main.java
@@ -0,0 +1,37 @@
+package _35.exercicio_dois;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Desafio 1036
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double numeroUm, numeroDois, numeroTres, delta, r1, r2;
+		
+		numeroUm = sc.nextDouble();
+		numeroDois = sc.nextDouble();
+		numeroTres = sc.nextDouble();
+		
+		delta = (Math.pow(numeroDois, 2)) - (4 * numeroUm * numeroTres);
+		r1 = ((numeroDois * -1) + (Math.sqrt(delta))) / (2 * numeroUm);
+		r2 = ((numeroDois * -1) - (Math.sqrt(delta))) / (2 * numeroUm);
+		
+		if (delta < 0 || numeroUm == 0) {
+			System.out.println("Impossivel calcular");
+		}
+		
+		else {
+			System.out.printf("R1 = %.5f%n", r1);
+			System.out.printf("R2 = %.5f%n", r2);
+		}
+		
+		sc.close();
+	}
+
+}
