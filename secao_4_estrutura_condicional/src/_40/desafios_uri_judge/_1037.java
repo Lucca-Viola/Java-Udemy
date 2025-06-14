4d60eadbadc9dc04a57fd77d3a64429ad31e43dd
Lucca Viola <luccaviola05@gmail.com>
2025-06-14 19:40:24 -0400

    Desafio 1037

---
+++ secao_4_estrutura_condicional/src/_40/desafios_uri_judge/_1037.java
@@ -0,0 +1,40 @@
+package _40.desafios_uri_judge;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class _1037 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double n = sc.nextDouble();
+		String intervalo = "";
+		
+		if (n >= 0 && n <= 100) {
+			if (n >= 0 && n <= 25.0000)
+				intervalo = "0,25";
+			else if (n >= 25.0001 && n <= 50.0000) {
+				intervalo = "25,50";
+			}
+			
+			else if (n >= 50.0001 && n <= 75.0000) {
+				intervalo = "50,75";
+			}
+			
+			else if (n >= 75.0001 && n <= 100) {
+				intervalo = "75,100";
+			}
+			
+			System.out.printf("Intervalo (%s)%n", intervalo);
+
+		}
+		
+		else {
+			System.out.println("Fora de intervalo");
+		}
+		
+	}
+
+}
